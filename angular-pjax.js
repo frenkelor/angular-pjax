0ddfa4c3203dce06c55b3e95238eb36138f33ec7
Dan Martens <dan@friendsoftheweb.com>
2015-02-01 14:22:46 -0500

    Initial commit

---
+++ angular-pjax.js
@@ -0,0 +1,59 @@
+(function() {
+
+  var angularPJAXModule = angular.module('ngPJAX', []);
+
+  var pjaxOptions = {
+    container: '[data-pjax-container]'
+  };
+
+  angularPJAXModule.provider('pjax', function() {
+    this.config = function(options) {
+      angular.extend(pjaxOptions, options);
+    };
+
+    this.$get = function() {
+      return {};
+    };
+  });
+
+  angularPJAXModule.directive('pjaxContainer', function() {
+    return {
+      controller: ['$rootScope', '$scope', '$window', '$compile', '$element', function($rootScope, $scope, $window, $compile, $element) {
+        var content, contentScope = null;
+
+        var updateCurrentPath = function() {
+          $rootScope.currentPath = $window.location.pathname;
+        };
+
+        $element.attr('data-pjax-container', true);
+
+        $(document).pjax('a:not([data-remote]):not([data-behavior]):not([data-skip-pjax])', pjaxOptions);
+
+        $(document).on('pjax:start', function() {
+          $rootScope.$apply(function() {
+            $rootScope.loading = true;
+          });
+        });
+
+        $(document).on('pjax:end', function() {
+          $rootScope.$apply(updateCurrentPath);
+
+          if (contentScope) {
+            contentScope.$destroy();
+          }
+
+          contentScope = $scope.$new(false, $scope);
+
+          $element.html($compile($element.html())(contentScope));
+
+          $rootScope.$apply(function() {
+            $rootScope.loading = false;
+          });
+        });
+
+        updateCurrentPath();
+      }]
+    };
+  });
+
+}).call(this);
